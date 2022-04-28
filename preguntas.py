46cf9cc457dba3bdad8e992de2d2cc70bab44f6c
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-04-27 22:02:07 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -136,7 +136,7 @@ def pregunta_04():
     # considerar 10 valores entre 0.1 y 1.0 para el parámetro alpha de
     # BernoulliNB.
     param_grid = {
-        "BernoulliNB__alpha": np.random.uniform(low=0.1, high=1.0, size=(10)),
+        "BernoulliNB__alpha": np.array((0.1, 1, 10)),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
